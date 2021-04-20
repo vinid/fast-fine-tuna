1e42d919fec0c317a49e133d835944f82b6a284c
vinid <chiccobia@gmail.com>
2021-04-20 08:57:20 +0200

    updates

--- fast_fine_tuna/fast_fine_tuna.py
+++ fast_fine_tuna/fast_fine_tuna.py
@@ -8,14 +8,16 @@ from torch.utils.data import DataLoader
 import os
 from tqdm import tqdm
 
+
 class FastFineTuna:
 
     def __init__(self, model_name, tokenizer_name):
         self.model_name = model_name
         self.tokenizer_name = tokenizer_name
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+    def cross_validate_fit(self, texts, labels, splits=5, epochs=5, batch_size=16, learning_rate=5e-5):
 
-    def cross_validate_fit(self, texts, labels, splits = 5, epochs=5, batch_size=16):
         config = AutoConfig.from_pretrained(self.model_name, num_labels=len(set(labels)),
                                             finetuning_task="custom")
         tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name)
@@ -30,34 +32,31 @@ class FastFineTuna:
         for train_index, test_index in skf.split(texts, labels):
             model = AutoModelForSequenceClassification.from_pretrained(self.model_name, config=config)
 
-
             X_train, X_test = texts[train_index].tolist(), texts[test_index].tolist()
             y_train, y_test = labels[train_index].tolist(), labels[test_index].tolist()
 
+            # not the smartest way to do this, but faster to code up
             tokenized_train = tokenizer(X_train, truncation=True, padding=True)
             tokenized_test = tokenizer(X_test, truncation=True, padding=True)
 
             train_dataset = MainDataset(tokenized_train, y_train)
             test_dataset = MainDataset(tokenized_test, y_test)
 
-            device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-            model.to(device)
+            model.to(self.device)
             model.train()
-
 
             train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-            optim = AdamW(model.parameters(), lr=5e-5)
+            optim = AdamW(model.parameters(), lr=learning_rate)
 
             pbar = tqdm(total=epochs, position=0, leave=True)
             for epoch in range(epochs):
                 pbar.update(1)
                 for batch in train_loader:
                     optim.zero_grad()
-                    input_ids = batch['input_ids'].to(device)
-                    attention_mask = batch['attention_mask'].to(device)
-                    lab = batch['labels'].to(device)
+                    input_ids = batch['input_ids'].to(self.device)
+                    attention_mask = batch['attention_mask'].to(self.device)
+                    lab = batch['labels'].to(self.device)
                     outputs = model(input_ids, attention_mask=attention_mask, labels=lab)
                     loss = outputs[0]
                     loss.backward()
@@ -65,20 +64,19 @@ class FastFineTuna:
             pbar.close()
             model.eval()
 
-            loader = DataLoader(test_dataset, batch_size=16)
+            loader = DataLoader(test_dataset, batch_size=batch_size)
             original.extend(y_test)
             with torch.no_grad():
                 for batch in loader:
-                    input_ids = batch['input_ids'].to(device)
-                    attention_mask = batch['attention_mask'].to(device)
+                    input_ids = batch['input_ids'].to(self.device)
+                    attention_mask = batch['attention_mask'].to(self.device)
 
                     outputs = model(input_ids, attention_mask=attention_mask)
                     predicted.extend(torch.argmax(outputs["logits"], axis=1).cpu().numpy().tolist())
             del model
         return original, predicted
 
-
-    def train_and_save(self, texts, labels, path, epochs=5):
+    def train_and_save(self, texts, labels, path, epochs=5, batch_size=16, learning_rate=5e-5):
         model = AutoModelForSequenceClassification.from_pretrained(self.model_name)
         tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name)
         labels = np.array(labels)
@@ -87,21 +85,19 @@ class FastFineTuna:
 
         train_dataset = MainDataset(tokenized_train, labels)
 
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-        model.to(device)
+        model.to(self.device)
         model.train()
 
-        train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-        optim = AdamW(model.parameters(), lr=5e-5)
+        optim = AdamW(model.parameters(), lr=learning_rate)
 
         for epoch in range(epochs):
             for batch in train_loader:
                 optim.zero_grad()
-                input_ids = batch['input_ids'].to(device)
-                attention_mask = batch['attention_mask'].to(device)
-                lab = batch['labels'].to(device)
+                input_ids = batch['input_ids'].to(self.device)
+                attention_mask = batch['attention_mask'].to(self.device)
+                lab = batch['labels'].to(self.device)
                 outputs = model(input_ids, attention_mask=attention_mask, labels=lab)
                 loss = outputs[0]
                 loss.backward()
@@ -110,6 +106,3 @@ class FastFineTuna:
         os.makedirs(path)
         model.save_pretrained(path)
         tokenizer.save_pretrained(path)
-
-
-

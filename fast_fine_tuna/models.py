ce3ee7cf19bb9a092e2f27b95702f2b05faa338c
vinid <chiccobia@gmail.com>
2021-05-19 18:51:07 +0200

    adding multitask

---
+++ fast_fine_tuna/models.py
@@ -0,0 +1,18 @@
+from transformers import AutoModel
+from torch import nn
+
+class MiniModel(nn.Module):
+    def __init__(self, model_name):
+        super().__init__()
+        self.model = AutoModel.from_pretrained(model_name)
+
+        self.first_classifier = nn.Linear(768, 2)
+        self.second_classifier = nn.Linear(768, 2)
+
+    def forward(self, input_ids, attention_mask):
+        output = self.model(input_ids, attention_mask=attention_mask)
+
+        A_logits = self.first_classifier(output[1])
+        B_logits = self.second_classifier(output[1])
+
+        return A_logits, B_logits
